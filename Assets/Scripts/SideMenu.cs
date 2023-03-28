3e6c2f3e9f8dd389c08c5d0977f0c5e1d2b5986a
Artem Filippov <90859140+RockefellerArtem@users.noreply.github.com>
2023-03-28 20:04:14 +0300

    [Project] Completed 2 task

--- Assets/Scripts/SideMenu.cs
+++ Assets/Scripts/SideMenu.cs
@@ -1,16 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class SideMenu : MonoBehaviour
 {
-
     [SerializeField] private Animator _animator;
 
+
+    [Header("Dialog Window")] 
+    [SerializeField] private GameObject _window;
+    [SerializeField] private TMP_InputField _inputName;
+    [SerializeField] private Button _enterButton;
+    [SerializeField] private TMP_Text _showInputNameText;
+
+    [SerializeField] private Button _showWindow;
+    
     private bool _isActive = true;
+
+    private string _name;
+
+    private void Start()
+    {
+        InitializationButton();
+    }
+
+    private void InitializationButton()
+    {
+        _enterButton.onClick.AddListener(InputName);
+        _showWindow.onClick.AddListener(HandlerClickShowButton);
+    }
 
 
     public void Switch() {
+        
         _isActive = !_isActive;
 
         if (_isActive) {
@@ -20,6 +46,27 @@ public class SideMenu : MonoBehaviour
             _animator.SetTrigger("Hide");
         }
 
+    }
+
+    private void InputName()
+    {
+        _name = _inputName.text;
+        
+        if (_name == null) return;
+        
+        _showInputNameText.text = _name;
+        
+        SetEnableWindow(false);
+    }
+
+    private void HandlerClickShowButton()
+    {
+        SetEnableWindow(true);
+    }
+
+    private void SetEnableWindow(bool isActive)
+    {
+        _window.SetActive(isActive);
     }
 
 }
