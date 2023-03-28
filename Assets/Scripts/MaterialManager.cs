fbf37314ba8c09f555632e128ee332a72316a290
Artem Filippov <90859140+RockefellerArtem@users.noreply.github.com>
2023-03-28 19:30:19 +0300

    [Project] Completed 1 task

---
+++ Assets/Scripts/MaterialManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour
+{
+
+    [SerializeField] private List<Renderer> _renderers;
+
+    public void SetMaterial(Material material) {
+        for (int i = 0; i < _renderers.Count; i++)
+        {
+            _renderers[i].material = material;
+        }
+    }
+    
+
+}
